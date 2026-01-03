5927d6fa531826892c4323886e2cceecc1043ab3
Anton <antontv2019@gmail.com>
2026-01-03 17:25:29 +0200

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "https://teaching.kse.org.ua/calendar/view.php?view=month&time=1764540000"
-response = requests.get(url)
-print(response.text)
